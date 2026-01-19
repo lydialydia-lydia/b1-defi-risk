279f928a5d1a08b9cbacfe3fc0814816b3756b53
lydialydia-lydia <yuxyawang@gmail.com>
2026-01-18 20:35:59 -0500

    Create risk_metrics.py
    Add risk metrics (health factor)

---
+++ src/risk_metrics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def compute_health_factor(
+    collateral_amount: pd.Series,
+    debt_amount: pd.Series,
+    price: float,
+    liquidation_threshold: float
+) -> pd.Series:
+    """
+    Compute Health Factor (HF).
+
+    HF = (collateral_value * LT) / debt_value
+    Here, debt_value = debt_amount (assume debt price = 1.0).
+    """
+    collateral_value = collateral_amount * price
+    hf = (collateral_value * liquidation_threshold) / debt_amount
+    return hf
